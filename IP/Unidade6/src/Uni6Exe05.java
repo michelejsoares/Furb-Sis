6c7faaa9bd168ce57809a92aed42dcb129819710
Conectar Sistemas Erp <148389106+michelejsoares@users.noreply.github.com>
2025-05-13 07:59:40 -0300

    commit

--- IP/Unidade6/src/Uni6Exe05.java
+++ IP/Unidade6/src/Uni6Exe05.java
@@ -4,12 +4,13 @@ public class Uni6Exe05 {
     public static void main(String[] args) {
         new Uni6Exe05();
     }
-    private int respostasR[] = new int[5], respostasM[] = new int[5], somaS = 0, somaN = 0, somaI = 0;
+    private int respostasR[] = new int[5], respostasM[] = new int[5], somaI = 0;
     private String perguntas[] = new String[5];
 
     private Uni6Exe05(){
         perguntas();
-        respostas();
+        respostasR();
+        respostasM();
         contagem();
     }
     private void perguntas(){
@@ -19,23 +20,34 @@ public class Uni6Exe05 {
         perguntas[3] = "Gosta de redes sociais?";
         perguntas[4] = "Gosta da Oktoberfest?";
     }
-    private void respostas(){
+    private void respostasR(){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Resposta Rapaz:");
         for(int i = 0; i < 5; i++){
             System.out.println(perguntas[i]+" 1 - SIM; 2 - NAO; 3 - IND (Indiferente)");
-            respostas[i] = sc.nextInt();
+            respostasR[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+
+    private void respostasM(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Resposta Rapaz:");
+        for(int i = 0; i < 5; i++){
+            System.out.println(perguntas[i]+" 1 - SIM; 2 - NAO; 3 - IND (Indiferente)");
+            respostasM[i] = sc.nextInt();
         }
         sc.close();
     }
 
     private void contagem(){
         for(int i = 0; i < 5; i++){
-            if(respostas[i] == 1){
-                somaS++;
-            }else if(respostas[i] == 2){
-                somaN++;
-            }else{
-                somaI++;
+            if(respostasR[i] == respostasM[i]){
+                somaI+= 3;
+            }else if(respostasR[i] == 3 || respostasM[i] == 3){
+                if(respostasR[i] == 1 || respostasR[i] == 2 || respostasM[i] == 1 || respostasM[i] == 2){
+                    somaI+= 1;
+                }
             }
         }
     }
